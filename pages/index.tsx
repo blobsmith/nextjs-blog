4e13c64f8af1e8a3d302d7d78a4f7c72117c7d1d
Olivier Gilbert <olivier.gilbert@ideative.ch>
2022-04-20 20:42:45 +0200

    fix simple quote error

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home({allPostsData}: Props) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I'm backend developer for more than 15 years as PHP/JavaScript expert </p>
+        <p>I&apos;m backend developer for more than 15 years as PHP/JavaScript expert </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
